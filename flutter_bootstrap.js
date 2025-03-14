dd8c431d1ba19fa9abd32374daf61329d3ed3c36
Arpit-Verma1 <arpitv747@gmail.com>
2025-03-14 18:42:42 +0530

    FEAT:Web Favicon, top header in task details, fix some ui bugs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2784730730"
+    serviceWorkerVersion: "2927325969"
   }
 });
