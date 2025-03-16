ddcc12290bd31288982b2157e7ad06b1a3d2cb4d
Arpit-Verma1 <arpitv747@gmail.com>
2025-03-16 20:35:46 +0530

    BUG:UI chanegs shared responsiblity model

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3051805457"
+    serviceWorkerVersion: "117360304"
   }
 });
